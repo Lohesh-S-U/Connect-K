e7bb742350ddf8f21398a10b5da3ba8ffb261dc4
Lohesh S U <sulohesh@gmail.com>
2024-09-30 19:52:42 +0530

    feat: Initial Commit

---
+++ game.py
@@ -0,0 +1,47 @@
+from ConnectState import ConnectState
+from mcts import MCTS
+
+
+def play():
+    k = int(input("Enter k : "))
+    print(f"Playing Connect-{k}\n\n")
+    state = ConnectState(k)
+    mcts = MCTS(state)
+
+    while not state.game_over():
+        print("Current state:")
+        state.print()
+
+        user_move = int(input("Enter a move: "))
+        while user_move not in state.get_legal_moves():
+            print("Illegal move")
+            user_move = int(input("Enter a move: "))
+
+        state.move(user_move)
+        mcts.move(user_move)
+
+        state.print()
+
+        if state.game_over():
+            print("Player one won!")
+            break
+
+        print("Thinking...")
+
+        mcts.search(8)
+        num_rollouts, run_time = mcts.statistics()
+        print("Statistics: ", num_rollouts, "rollouts in", run_time, "seconds")
+        move = mcts.best_move()
+
+        print("MCTS chose move: ", move)
+
+        state.move(move)
+        mcts.move(move)
+
+        if state.game_over():
+            print("Player two won!")
+            break
+
+
+if __name__ == "__main__":
+    play()
